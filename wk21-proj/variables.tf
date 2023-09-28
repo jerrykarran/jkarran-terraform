1f0aafbd79bc7ec9a164a5edcea33d2d633b9d09
Jerry Karran <jerrykarran@gmail.com>
2023-09-28 23:53:54 +0000

    adding project files

---
+++ wk21-proj/variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+# variable "my_keypair" {
+#   type    = string
+#   default = "LUIT_TEST_KEYS.pem"
+# }
+
+variable "my_instance_type" {
+  description = "Type of instance for autoscaling group"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_for_web_server" {
+  description = "ami for the web server"
+  type        = string
+  default     = "ami-055864024342e0e66"
+}
